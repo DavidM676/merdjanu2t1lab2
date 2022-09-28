5f5ffeae8e4cc617e8013317ace870ffc8356d4b
1E10 Laptop <sample@bths.edu>
2022-09-28 11:15:47 -0400

    initial commit

---
+++ src/CircleRunner.java
@@ -0,0 +1,16 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle c1 = new Circle(5.0);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        c1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        //My area is 78.5
+
+        // D. run the code and confirm your prediction! Were you correct?
+        //yes
+    }
+}
